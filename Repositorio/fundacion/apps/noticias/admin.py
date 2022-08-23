b4ce9343de46d6529f12f9149b5a307271b6d581
Tiago Klatt <tiiagoklatt@gmail.com>
2022-08-23 10:14:20 -0300

    Listar noticias

--- Repositorio/fundacion/apps/noticias/admin.py
+++ Repositorio/fundacion/apps/noticias/admin.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
 from .models import Noticia
 
-<<<<<<< HEAD
+
 from . models import Noticia
 
-=======
->>>>>>> 925944ac1aca9ed30a60a98a2f1cd0688426e669
+
 admin.site.register(Noticia)
